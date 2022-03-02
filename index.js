d73b6aba2c5cd2fb1b558d4681534106488e7029
Lakshmi5524 <webtech5524@gmail.com>
2022-03-02 23:51:23 +0530

    update

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
 const port = process.env.PORT || 5000
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const userRoute = require("./routes/users");
-const pinRoute = require("./routes/pins");
-const cors = require('cors')
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const userRoute = require("./routes/users")
+const pinRoute = require("./routes/pins")
+const cors = require("cors")
 app.use(cors())
 
-dotenv.config();
+dotenv.config()
 
-app.use(express.json());
+app.use(express.json())
 
 mongoose
-	.connect(process.env.MONGO_URL, {
-		useNewUrlParser: true,
-	})
-	.then(() => console.log("MongoDB connected!"))
-	.catch(err => console.log(err));
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("MongoDB connected!"))
+  .catch((err) => console.log(err))
 
-app.use("/api/users", userRoute);
-app.use("/api/pins", pinRoute);
-
-//Server Production Assests
-// if (process.env.NODE_ENV === "production") {
-// 	app.use('/', express.static(path.join("client/build")))
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-// 	})
-// }
+app.use("/api/users", userRoute)
+app.use("/api/pins", pinRoute)
 
 app.listen(port, () => {
-	console.log(`Backend server is running in port ${port}`);
-});
+  console.log(`Backend server is running in port ${port}`)
+})
